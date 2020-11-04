24a31684037c916f9f2602e84314afc7286914bc
dahhmani <dahhmani@hotmail.com>
2020-11-04 02:05:24 -0500

    [Task 2.7] Implemented tracker class [include/tracker.h] modified [app/tracker.cpp] modified

--- include/tracker.h
+++ include/tracker.h
@@ -36,6 +36,7 @@
 #include <vector>
 #include <unordered_set>
 #include <map>
+#include <tuple>
 #include <opencv2/dnn.hpp>
 
 /**
@@ -67,28 +68,35 @@ class ObjectTracker {
    * @param pixel in the image
    * @return 3D location in the robot coordinate system
    */
-  cv::Point3f localizeObjectKeypoint(const cv::Point2i& object) const;
+  cv::Point3f localize(const cv::Point2i& objectKeypoint) const;
 
   /**
-   * @brief Function for detecting objects' keypoints in a given frame
-   * Extracts a single keypoint from every bounding box detection.
+   * @brief Function for detecting objects in a given frame
    * @param frame
-   * @return vector of the objects' keypoints in the image
+   * @return classIds, confidences, bounding boxes
    */
-  std::vector<cv::Point2i> detectObjectKeypoints(cv::Mat frame);
+  std::tuple<std::vector<int>, std::vector<float>, std::vector<cv::Rect>>
+  detectObjects(cv::Mat frame);
 
   /**
-   * @brief Variable for holding the classes of the pre-trained detection model 
+   * @brief Getter method for the classes of the pre-trained detection model
+   * @param void
+   * @return vector of the the dataset classes
    */
-  std::vector<std::string> datasetLabels_;
+  std::vector<std::string> datasetLabels() const;
 
  private:
+  void visualize(cv::Mat frame, int classId, float confidence,
+                 const cv::Rect& box, const cv::Point2i& objectKeypoint,
+                 const cv::Point3f& objectLocation, float distance);
   std::vector<std::string> parseFile(const std::string& fileName) const;
 
   std::unordered_set<std::string> objectClasses_;
   float minConfidence_, minOverlap_;
-  /*cv::dnn::DetectionModel network_{"../yolo/yolov4.weights",
-                                   "../yolo/yolov4.cfg"};*/
+  std::vector<std::string> datasetLabels_;
+  cv::dnn::DetectionModel network_{
+      "../models/yolo/yolov4.weights",
+      "../models/yolo/yolov4.cfg"};
   cv::Matx34f P_;  // camera calibration matrix
   std::map<std::string, cv::Scalar> colors_;
 };
